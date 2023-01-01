fc9cd7c8ff3e40a90ae25e2954c8d6121a393688
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-01-01 14:33:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+function updateDOM(e){
+    document.getElementsByClassName("realgame")[0].innerHTML = e
+}
+function RandomInt(){
+    return Math.floor(Math.random() * 100)
+}
+
+function updateBallLocation(){
+    document.getElementById("ball").style.top = `${RandomInt()}%`;
+    document.getElementById("ball").style.left = `${RandomInt()}%`;
+}
+localStorage.setItem("speed", "1000")
+let GameStorage = localStorage.getItem("speed")
+const menu = `
+<div class="menu">
+            <div class="box">
+                <h1 class="gamename">Clicker</h1>
+                <span class="word">Because I'm Board</span>
+        
+                <div class="bottons">
+                    <button class="startgame" onclick="StartGame()">Start Game</button>
+                    <button class="settings">Settings</button>
+                </div>
+            </div>
+        </div>
+`;
+
+const game = `<div class="playpage">
+<div class="header">
+    <p> Score: <span id="score">12</span></p>
+    <p> highiest Score: <span id="high">234</span></p>
+    <button class="endgame" onclick="goToMenu()">
+        End
+    </button>
+    <hr>
+</div>
+<div class="clickes">
+    <div class="ball" id="ball" onclick="increaseScore()"></div>
+</div>
+</div>
+
+`
+function changeGamePage(pagename){
+    if (pagename == "menu")
+        updateDOM(menu)
+
+    if (pagename == "game")
+        updateDOM(game)
+}
+
+let ballRunner
+
+function StartGame(){
+    changeGamePage("game")
+    ballRunner = setInterval(() => {
+        updateBallLocation()
+    },  parseInt(GameStorage))
+}
+
+function goToMenu(){
+    clearInterval(ballRunner) 
+    changeGamePage("menu")
+}
+
+function increaseScore(){
+
+}
